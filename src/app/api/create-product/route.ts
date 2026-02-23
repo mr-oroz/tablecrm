e3f7b6fb85a252f64a5786fda1a22f9c0e063226
Oroz <oroz7307@gmail.com>
2026-02-23 17:46:21 +0600

    token add .env

--- src/app/api/create-product/route.ts
+++ src/app/api/create-product/route.ts
@@ -2,9 +2,10 @@ import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   const body = await req.json()
+  const token = process.env.TABLECRM_TOKEN
 
   const response = await fetch(
-    "https://app.tablecrm.com/api/v1/nomenclature?token=af1874616430e04cfd4bce30035789907e899fc7c3a1a4bb27254828ff304a77",
+    `https://app.tablecrm.com/api/v1/nomenclature?token=${token}`,
     {
       method: "POST",
       headers: {
